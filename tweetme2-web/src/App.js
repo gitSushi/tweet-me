3de0d8bec794d1908989878f11dbcc437cebc165
gitSushi <lossushi37@gmail.com>
2020-10-19 09:25:43 +0200

    some upgrades

--- tweetme2-web/src/App.js
+++ tweetme2-web/src/App.js
@@ -1,32 +1,32 @@
-import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function loadTweets(callback){
-      const xhr = new XMLHttpRequest();
-      const method = 'GET';
-      const url = "http://127.0.0.1:8080/api/tweets";
-      const responseType = "json";
+function loadTweets(callback) {
+  const xhr = new XMLHttpRequest();
+  const method = "GET";
+  const url = "http://127.0.0.1:8080/api/tweets";
+  const responseType = "json";
 
-      xhr.responseType = responseType;
-      xhr.open(method, url);
-      xhr.onload = function(){
-        callback(xhr.response, xhr.status);
-      };
-      xhr.onerror = function(e){
-        console.log(e);
-        callback({message: "the request was an error"}, 400)
-      };
-      xhr.send();
-    }
+  xhr.responseType = responseType;
+  xhr.open(method, url);
+  xhr.onload = function() {
+    callback(xhr.response, xhr.status);
+  };
+  xhr.onerror = function(e) {
+    console.log(e);
+    callback({ message: "the request was an error" }, 400);
+  };
+  xhr.send();
+}
 
 function App() {
   const [tweets, setTweets] = useState([]);
-  
+
   useEffect(() => {
     const myCallback = (response, status) => {
       console.log(response, status);
-      if(status === 200){
+      if (status === 200) {
         setTweets(response);
       }
     };
@@ -43,10 +43,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <ul>
-        {tweets.map((tweet, index) => (
-          <li key={index}>{tweet.content}</li>
-        ))}
-      </ul>
+          {tweets.map((tweet, index) =>
+            <li key={index}>
+              {tweet.content}
+            </li>
+          )}
+        </ul>
       </header>
     </div>
   );
